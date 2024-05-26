e9b31fe780434189ef60c71a7c23dd290c76c62c
nrusnac <nrusnac@tudelft.nl>
2024-05-26 16:15:57 +0200

    Trying to fix seccrets

--- src/app.py
+++ src/app.py
@@ -88,21 +88,21 @@ def fetch_model():
         repo="https://github.com/remla24-team12/model-training.git"
     )
 
-    config = {
-        'remote': {
-            'gdrive': {
-                'gdrive_service_account_json_file_path': './src/remla-team-12-2078257eb673.json',
-                'gdrive_client_id': secrets["CLIENT_ID"],
-                'gdrive_client_secret': secrets["CLIENT_SECRET"],
-                'gdrive_use_service_account': True  # Use True as a boolean, not a string
-            }
-        }
-    }
+    # config = {
+    #     'remote': {
+    #         'gdrive': {
+    #             'gdrive_service_account_json_file_path': './src/remla-team-12-2078257eb673.json',
+    #             'gdrive_client_id': secrets["CLIENT_ID"],
+    #             'gdrive_client_secret': secrets["CLIENT_SECRET"],
+    #             'gdrive_use_service_account': True  # Use True as a boolean, not a string
+    #         }
+    #     }
+    # }
 
     fs = dvc.api.DVCFileSystem(
         url='https://github.com/remla24-team12/model-training.git',
         rev=artifact['rev'],
-        config=config
+        # config=config
     )
 
     fs.get_file(artifact['path'], os.path.join("src","model",os.path.basename(artifact['path'])))
