cdcb2d8380b287f8f277c69cc5cfc553271c557e
Reinout Vos <r.h.a.vos@student.tudelft.nl>
2024-06-03 19:59:22 +0200

    added functionality for new labels

--- src/app.py
+++ src/app.py
@@ -1,50 +1,34 @@
-import os
-import json
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flasgger import Swagger
 from flask_cors import CORS
-import dvc.api
+from prometheus_flask_exporter import PrometheusMetrics
 import numpy as np
 from keras.models import load_model
 from lib_ml import preprocessing
+from fetch_model import fetch_model
+from dvc_commands import pull_from_dvc, push_to_dvc
 import json
 import os
 
 app = Flask(__name__)
 swagger = Swagger(app)
+metrics = PrometheusMetrics(app)
 CORS(app)
 
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return jsonify({"message": "The API is running"})
+
+
 @app.route('/predict', methods=['POST'])
+@metrics.counter('total_requests', 'Total number of requests')
+@metrics.gauge('active_requests', 'Number of active requests')
+@metrics.histogram('predict_request_duration_seconds', 'Histogram of request durations in seconds')
 def predict():
     """
-    Make a hardcoded prediction
-    ---
-    consumes:
-      - application/json
-    parameters:
-      - name: input_data
-        in: body
-        description: URL to be classified.
-        required: true
-        schema:
-          type: object
-          properties:
-            url:
-              type: string
-              example: https://www.example.org/bone.html
-    responses:
-      200:
-        description: Some result
-        schema:
-          type: object
-          properties:
-            result:
-              type: object
-            safe:
-              type: boolean
-
+    Receives a URL, makes a phishing/valid prediction, returns result
     """
-
     model = load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model/model.keras'))
 
     data = request.get_json()
@@ -53,59 +37,31 @@ def predict():
     y_pred = model.predict(input_data, batch_size=1000)
     y_pred_binary = (np.array(y_pred) <= 0.5).astype(int)
 
-    print(y_pred_binary)
-
     return json.dumps({"result": y_pred.tolist()[0], "safe": y_pred_binary.tolist()[0]})
 
-def fetch_model():
-    """Fetch model and tokenizer from dvc registry"""
 
-    # Get the absolute path of the directory containing the current script
-    # script_dir = os.path.dirname(os.path.abspath(__file__))
+@app.route('/label', methods=['POST'])
+def new_label():
+    """
+    Receives a URL and a user-provided label.
+    Adds the label to the 'better_labels.txt' and pushes this to the remote using dvc. 
+    """
+    data = request.get_json()
+    url = data.get('url')
+    new_label = data.get('newLabel')
 
-    # Change the current working directory to the directory of the current script
-    # os.chdir(script_dir)
+    # Ensure the required file is pulled from Google Drive using DVC
+    pull_from_dvc()
 
-    print("Current Working Directory:", os.getcwd())
+    # Add new entry to the file
+    file_path = 'data/better_labels.txt'
+    with open(file_path, 'a') as file:
+        file.write(f"{new_label}\t{url}\n")
 
-    directory_contents = os.listdir('.')
+    # Push updated file to DVC
+    push_to_dvc()
 
-    # Print each item in the directory
-    for item in directory_contents:
-        print(item)
-
-
-    secrets = load_secrets()
-
-    
-    artifact = dvc.api.artifacts_show(
-        'phishing-detection',
-        repo="https://github.com/remla24-team12/model-training.git"
-    )
-
-    config = {
-        'remote': {
-            'gdrive': {
-                'gdrive_service_account_json_file_path': './src/remla-team-12-2078257eb673.json',
-                'gdrive_client_id': secrets["CLIENT_ID"],
-                'gdrive_client_secret': secrets["CLIENT_SECRET"],
-                'gdrive_use_service_account': True  # Use True as a boolean, not a string
-            }
-        }
-    }
-
-    fs = dvc.api.DVCFileSystem(
-        url='https://github.com/remla24-team12/model-training.git',
-        rev=artifact['rev'],
-        config=config
-    )
-
-    fs.get_file(artifact['path'], os.path.join("src","model",os.path.basename(artifact['path'])))
-
-def load_secrets(filename='./src/secrets.json'):
-    with open(filename, 'r') as file:
-        secrets = json.load(file)
-    return secrets
+    return jsonify({"message": "Data received", "url": url, "newLabel": new_label})
 
 
 if __name__ == "__main__":
